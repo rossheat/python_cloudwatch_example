67112b7320dbb785c8dc0370548cac9faef8fcc7
Ross Heaton <rossalanheaton@gmail.com>
2023-12-24 06:16:48 +0000

    Reorganise main

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from cloud_logger import logger
 
+log = logger(__name__)
+
 
 def main():
-    print("Starting main")
-    log = logger(__name__)
     log.info("Hello from main")
-    print("Ending main")
 
 
 if __name__ == "__main__":
